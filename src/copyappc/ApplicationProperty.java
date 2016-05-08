ae312fbc940f5d5d550472740be293fcc1a02eab
Win10Lxp <xplixina@sina.com>
2016-05-08 18:16:37 +0800

    增加属性了

---
+++ src/copyappc/ApplicationProperty.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package copyappc;
+
+/**
+ *
+ * @author Win10Lxp
+ */
+public class ApplicationProperty {
+
+    private String systemDB;
+    private String appPath;
+    private String appName;
+    private String userDB;
+
+    /**
+     * @return the systemDB
+     */
+    public String getSystemDB() {
+        return systemDB;
+    }
+
+    /**
+     * @param systemDB the systemDB to set
+     */
+    public void setSystemDB(String systemDB) {
+        this.systemDB = systemDB;
+    }
+
+    /**
+     * @return the appPath
+     */
+    public String getAppPath() {
+        return appPath;
+    }
+
+    /**
+     * @param appPath the appPath to set
+     */
+    public void setAppPath(String appPath) {
+        this.appPath = appPath;
+    }
+
+    /**
+     * @return the appName
+     */
+    public String getAppName() {
+        return appName;
+    }
+
+    /**
+     * @param appName the appName to set
+     */
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    /**
+     * @return the userDB
+     */
+    public String getUserDB() {
+        return userDB;
+    }
+
+    /**
+     * @param userDB the userDB to set
+     */
+    public void setUserDB(String userDB) {
+        this.userDB = userDB;
+    }
+    
+}
